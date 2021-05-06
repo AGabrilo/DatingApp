7decc6481326aa3c7b10c54023442238867e70d7
AGabrilo <agabri00@fesb.hr>
2021-05-06 13:53:12 +0200

    End of section 9

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -12,12 +12,11 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {} 
-  
 
-  constructor(public accountService: AccountService,private router: Router,private toastr:ToastrService) { }
+  constructor(public accountService: AccountService,private router: Router,
+    private toastr:ToastrService) { }
 
   ngOnInit(): void {
-   
   }
 
   login(){
